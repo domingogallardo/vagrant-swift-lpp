19bcc74190abd0aac46ba7b85003a7b95b3c22f9
Domingo Gallardo <domingo.gallardo@ua.es>
2017-03-17 08:44:32 +0100

    Primer commit

---
+++ holaMundo.swift
@@ -0,0 +1,16 @@
+// Primer programa Swift
+
+var str = "Mensaje"
+str = "Hola mundo!"
+print(str)
+
+// Vamos a sumar dos números
+
+var firstNumber = 2
+var secondNumber = 3
+var totalSum = firstNumber + secondNumber
+firstNumber = firstNumber + 1
+secondNumber = secondNumber + 1
+totalSum = firstNumber + secondNumber
+print("El resultado de la suma es = \(totalSum)")
+
